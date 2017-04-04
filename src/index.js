cca52aaa1c4d375740bf7b6c2571fcbda845d7a5
nsxz945 <nsxz@qq.com>
2017-04-04 15:40:54 +0800

    modify folder position

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
+import 'bulma/css/bulma.css'
 ReactDOM.render(
-  <h1>Hello, World!</h1>,
+ <div className="notification is-info">Hello, Wold!</div>,
   document.getElementById('root2')
 );
