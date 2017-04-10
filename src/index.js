bd6983031c0ce5b29dca3b3f0d44c8aa7c3c186e
nsxz945 <nsxz@qq.com>
2017-04-10 16:58:25 +0800

    component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import 'bulma/css/bulma.css'
+import Welcome from './Welcome'
+
 ReactDOM.render(
- <App />,
+  <Welcome/>,
   document.getElementById('container')
-);
+)
