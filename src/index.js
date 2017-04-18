edc91dc3d205be30cbf4a44a2f98196268ca3db8
nsxz945 <nsxz@qq.com>
2017-04-18 22:17:20 +0800

    添加label

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
+import UserDialog from './UserDialog'
 ReactDOM.render(
   <App />,
+  document.getElementById('root')
+)
+ReactDOM.render(
+  <UserDialog />,
   document.getElementById('root')
 )
